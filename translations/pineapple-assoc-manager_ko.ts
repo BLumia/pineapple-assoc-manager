49d1b763a8d96d5c31785b50bf0962d819d96912
Hosted Weblate <hosted@weblate.org>
2025-12-01 18:00:19 +0100

    i18n: Translations update from Hosted Weblate
    Co-authored-by: Andrey <andrey@mailbox.org>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Languages add-on <noreply-addon-languages@weblate.org>
    Co-authored-by: VenusGirl <VenusGirl@outlook.com>
    Translate-URL: https://hosted.weblate.org/projects/pineapple-pictures/assoc-manager/ko/
    Translate-URL: https://hosted.weblate.org/projects/pineapple-pictures/assoc-manager/ru/
    Translation: pineapple-pictures/File Association Manager

---
+++ translations/pineapple-assoc-manager_ko.ts
@@ -0,0 +1,137 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko" sourcelanguage="en">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../app/mainwindow.ui" line="14"/>
+        <source>File Association Manager</source>
+        <translation>파일 연결 관리자</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.ui" line="33"/>
+        <source>Check All</source>
+        <translation>모두 선택</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.ui" line="40"/>
+        <source>Uncheck All</source>
+        <translation>모두 선택 해제</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.ui" line="60"/>
+        <location filename="../app/mainwindow.cpp" line="107"/>
+        <source>Help</source>
+        <translation>도움말</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.ui" line="67"/>
+        <location filename="../app/mainwindow.cpp" line="70"/>
+        <source>System Settings</source>
+        <translation>시스템 설정</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.ui" line="74"/>
+        <source>Register Association</source>
+        <translation>연결 등록</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="27"/>
+        <source>File Association Manager for %1</source>
+        <translation>%1에 대한 파일 연결 관리자</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="38"/>
+        <source>Registered: %1 | Associated: %2 format(s)</source>
+        <translation>등록됨: %1 | 연결됨: %2 형식</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="39"/>
+        <source>Yes</source>
+        <translation>예</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="39"/>
+        <source>No</source>
+        <translation>아니요</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="69"/>
+        <location filename="../app/mainwindow.cpp" line="79"/>
+        <source>Success</source>
+        <translation>성공</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="74"/>
+        <source>Skip</source>
+        <translation>건너뛰기</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="75"/>
+        <source>Associations information updated successfully.</source>
+        <translation>연결 정보가 성공적으로 업데이트되었습니다.</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="76"/>
+        <source>You might also want to open System Settings&apos; default apps management page. Open now?</source>
+        <translation>시스템 설정의 기본 앱 관리 페이지를 열어보세요. 지금 열까요?</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="79"/>
+        <source>Associations information removed successfully.</source>
+        <translation>연결 정보가 성공적으로 제거되었습니다.</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="94"/>
+        <source>This program helps you register file type assoication information and capabilities to system register.</source>
+        <translation>이 프로그램은 파일 유형 연결 정보와 기능을 시스템 레지스터에 등록하는 데 도움이 됩니다.</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="95"/>
+        <source>When you click the &quot;Register Association&quot; button, if you have checked at least one file type association, this program will
+register `%1` to your system, with the file type association capabilities you&apos;ve selected.</source>
+        <translation>&quot;연결 등록&quot; 버튼을 클릭하면, 하나 이상의 파일 유형 연결을 선택한 경우, 이 프로그램은
+선택한 파일 유형 연결 기능을 사용하여 `%1`을 시스템에 등록합니다.</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="97"/>
+        <source>Due to Windows 10+ limitation, **if you already associated the selected formats with other program, then we cannot directly modify
+existing file type association for you**, so after you done the registration by clicking &quot;Register Association&quot;, you can then click
+the &quot;System Settings&quot; button to directly open the system control panel for `%1`, so you can manage the file type association there.</source>
+        <translation>Windows 10+ 제한으로 인해 **선택한 형식을 이미 다른 프로그램과 연결한 경우 기존 파일 형식 연결을
+직접 수정할 수 없습니다**. 따라서 &quot;연결 등록&quot;을 클릭하여 등록을 완료한 후 &quot;시스템 설정&quot; 버튼을
+클릭하여&apos;%1&apos;에 대한 시스템 제어판을 직접 열 수 있으므로 파일 형식 연결을 관리할 수 있습니다.</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="100"/>
+        <source>When you want to remove the system registration for `%1`, you can simply uncheck all file types on the left-hand side,
+then click &quot;Register Association&quot; again. It will remove all related information from the system registery.</source>
+        <translation>&apos;%1&apos;에 대한 시스템 등록을 제거하려면 왼쪽의 모든 파일 유형을 선택 취소한 다음
+&quot;연결 등록&quot;을 다시 클릭하면 됩니다. 시스템 등록에서 모든 관련 정보가 제거됩니다.</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../app/main.cpp" line="23"/>
+        <source>Pineapple Assoc Manager</source>
+        <translation>Pineapple 연결 관리자</translation>
+    </message>
+    <message>
+        <location filename="../app/main.cpp" line="31"/>
+        <source>Path to configuration file.</source>
+        <translation>구성 파일의 경로입니다.</translation>
+    </message>
+    <message>
+        <location filename="../app/main.cpp" line="32"/>
+        <location filename="../app/main.cpp" line="36"/>
+        <source>file</source>
+        <translation>파일</translation>
+    </message>
+    <message>
+        <location filename="../app/main.cpp" line="35"/>
+        <source>Path to .pademo/.patest file.</source>
+        <translation>.pademo/.patest 파일의 경로입니다.</translation>
+    </message>
+</context>
+</TS>
