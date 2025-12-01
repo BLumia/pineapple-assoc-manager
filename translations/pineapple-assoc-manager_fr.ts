058ae85a1a25f24ee47c1cb03709ac7ee647ca5a
Hosted Weblate <hosted@weblate.org>
2025-12-01 18:00:19 +0100

    i18n: Translations update from Hosted Weblate
    Co-authored-by: Andrey <andrey@mailbox.org>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Languages add-on <noreply-addon-languages@weblate.org>
    Co-authored-by: VenusGirl <VenusGirl@outlook.com>
    Translate-URL: https://hosted.weblate.org/projects/pineapple-pictures/assoc-manager/ko/
    Translate-URL: https://hosted.weblate.org/projects/pineapple-pictures/assoc-manager/ru/
    Translation: pineapple-pictures/File Association Manager

---
+++ translations/pineapple-assoc-manager_fr.ts
@@ -0,0 +1,133 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr" sourcelanguage="en">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../app/mainwindow.ui" line="14"/>
+        <source>File Association Manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.ui" line="33"/>
+        <source>Check All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.ui" line="40"/>
+        <source>Uncheck All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.ui" line="60"/>
+        <location filename="../app/mainwindow.cpp" line="107"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.ui" line="67"/>
+        <location filename="../app/mainwindow.cpp" line="70"/>
+        <source>System Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.ui" line="74"/>
+        <source>Register Association</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="27"/>
+        <source>File Association Manager for %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="38"/>
+        <source>Registered: %1 | Associated: %2 format(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="39"/>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="39"/>
+        <source>No</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="69"/>
+        <location filename="../app/mainwindow.cpp" line="79"/>
+        <source>Success</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="74"/>
+        <source>Skip</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="75"/>
+        <source>Associations information updated successfully.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="76"/>
+        <source>You might also want to open System Settings&apos; default apps management page. Open now?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="79"/>
+        <source>Associations information removed successfully.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="94"/>
+        <source>This program helps you register file type assoication information and capabilities to system register.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="95"/>
+        <source>When you click the &quot;Register Association&quot; button, if you have checked at least one file type association, this program will
+register `%1` to your system, with the file type association capabilities you&apos;ve selected.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="97"/>
+        <source>Due to Windows 10+ limitation, **if you already associated the selected formats with other program, then we cannot directly modify
+existing file type association for you**, so after you done the registration by clicking &quot;Register Association&quot;, you can then click
+the &quot;System Settings&quot; button to directly open the system control panel for `%1`, so you can manage the file type association there.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="100"/>
+        <source>When you want to remove the system registration for `%1`, you can simply uncheck all file types on the left-hand side,
+then click &quot;Register Association&quot; again. It will remove all related information from the system registery.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../app/main.cpp" line="23"/>
+        <source>Pineapple Assoc Manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/main.cpp" line="31"/>
+        <source>Path to configuration file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/main.cpp" line="32"/>
+        <location filename="../app/main.cpp" line="36"/>
+        <source>file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../app/main.cpp" line="35"/>
+        <source>Path to .pademo/.patest file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
