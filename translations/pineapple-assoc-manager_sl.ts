0bdd0d228e58fc8a4f4fd48bc776809ca92fce6b
Hosted Weblate <hosted@weblate.org>
2026-01-25 02:53:36 +0100

    i18n: Translations update from Hosted Weblate
    Co-authored-by: Andrej Poženel - SDT <anderlli0053@live.com>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Languages add-on <noreply-addon-languages@weblate.org>
    Translate-URL: https://hosted.weblate.org/projects/pineapple-pictures/assoc-manager/sl/
    Translation: pineapple-pictures/File Association Manager

---
+++ translations/pineapple-assoc-manager_sl.ts
@@ -0,0 +1,137 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sl" sourcelanguage="en">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../app/mainwindow.ui" line="14"/>
+        <source>File Association Manager</source>
+        <translation>Upravitelj povezav datotek</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.ui" line="33"/>
+        <source>Check All</source>
+        <translation>Označi vse</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.ui" line="40"/>
+        <source>Uncheck All</source>
+        <translation>Odznači vse</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.ui" line="60"/>
+        <location filename="../app/mainwindow.cpp" line="107"/>
+        <source>Help</source>
+        <translation>Pomoč</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.ui" line="67"/>
+        <location filename="../app/mainwindow.cpp" line="70"/>
+        <source>System Settings</source>
+        <translation>Sistemske nastavitve</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.ui" line="74"/>
+        <source>Register Association</source>
+        <translation>Registriraj povezave datotek</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="27"/>
+        <source>File Association Manager for %1</source>
+        <translation>Upravitelj povezav datotek za %1</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="38"/>
+        <source>Registered: %1 | Associated: %2 format(s)</source>
+        <translation>Registrirano: %1 | Povezan: %2 format(ov)</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="39"/>
+        <source>Yes</source>
+        <translation>Da</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="39"/>
+        <source>No</source>
+        <translation>Ne</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="69"/>
+        <location filename="../app/mainwindow.cpp" line="79"/>
+        <source>Success</source>
+        <translation>Uspeh</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="74"/>
+        <source>Skip</source>
+        <translation>Preskoči</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="75"/>
+        <source>Associations information updated successfully.</source>
+        <translation>Podatki o združenjih so bili uspešno posodobljeni.</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="76"/>
+        <source>You might also want to open System Settings&apos; default apps management page. Open now?</source>
+        <translation>Morda boste želeli odpreti tudi privzeto stran za upravljanje aplikacij v sistemskih nastavitvah. Odprem zdaj?</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="79"/>
+        <source>Associations information removed successfully.</source>
+        <translation>Podatki o povezavah so bili uspešno odstranjeni.</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="94"/>
+        <source>This program helps you register file type assoication information and capabilities to system register.</source>
+        <translation>Ta program vam pomaga registrirati informacije o povezavah tipov datotek in zmožnosti za registracijo sistema.</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="95"/>
+        <source>When you click the &quot;Register Association&quot; button, if you have checked at least one file type association, this program will
+register `%1` to your system, with the file type association capabilities you&apos;ve selected.</source>
+        <translation>Ko kliknete gumb &quot;Registriraj povezavo&quot; in ste označili vsaj eno povezavo vrste datoteke, bo ta program
+registriral `%1` v vaš sistem z izbranimi možnostmi povezave vrste datoteke.</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="97"/>
+        <source>Due to Windows 10+ limitation, **if you already associated the selected formats with other program, then we cannot directly modify
+existing file type association for you**, so after you done the registration by clicking &quot;Register Association&quot;, you can then click
+the &quot;System Settings&quot; button to directly open the system control panel for `%1`, so you can manage the file type association there.</source>
+        <translation>Zaradi omejitev sistema Windows 10+, **če ste izbrane formate že povezali z drugim programom, ne moremo neposredno spremeniti
+obstoječe povezave vrste datotek namesto vas**, zato lahko po registraciji s klikom na &quot;Registriraj povezavo&quot; kliknete
+gumb &quot;Sistemske nastavitve&quot;, da neposredno odprete nadzorno ploščo sistema za `%1`, kjer lahko tam upravljate povezavo vrste datotek.</translation>
+    </message>
+    <message>
+        <location filename="../app/mainwindow.cpp" line="100"/>
+        <source>When you want to remove the system registration for `%1`, you can simply uncheck all file types on the left-hand side,
+then click &quot;Register Association&quot; again. It will remove all related information from the system registery.</source>
+        <translation>Ko želite odstraniti registracijo sistema za `%1`, lahko preprosto počistite vse vrste datotek na levi strani
+in nato znova kliknete &quot;Registriraj povezavo&quot;. S tem boste odstranili vse povezane informacije iz sistemskega registra.</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../app/main.cpp" line="23"/>
+        <source>Pineapple Assoc Manager</source>
+        <translation>Upravitelj povezav datotek Pineapple</translation>
+    </message>
+    <message>
+        <location filename="../app/main.cpp" line="31"/>
+        <source>Path to configuration file.</source>
+        <translation>Pot do konfiguracijske datoteke.</translation>
+    </message>
+    <message>
+        <location filename="../app/main.cpp" line="32"/>
+        <location filename="../app/main.cpp" line="36"/>
+        <source>file</source>
+        <translation>datoteka</translation>
+    </message>
+    <message>
+        <location filename="../app/main.cpp" line="35"/>
+        <source>Path to .pademo/.patest file.</source>
+        <translation>Pot do datoteke .pademo/.patest.</translation>
+    </message>
+</context>
+</TS>
