c206f9b59b4c26fe57b3aa9b0ea728d56a15d0f4
Hosted Weblate <hosted@weblate.org>
2026-03-24 11:09:49 +0100

    i18n: Translations update from Hosted Weblate
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Translate-URL: https://hosted.weblate.org/projects/pineapple-pictures/assoc-manager/nl/
    Translation: pineapple-pictures/File Association Manager

--- translations/pineapple-assoc-manager_nl.ts
+++ translations/pineapple-assoc-manager_nl.ts
@@ -6,104 +6,108 @@
     <message>
         <location filename="../app/mainwindow.ui" line="14"/>
         <source>File Association Manager</source>
-        <translation type="unfinished"></translation>
+        <translation>Bestandstoewijzingen beheren</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.ui" line="33"/>
         <source>Check All</source>
-        <translation type="unfinished"></translation>
+        <translation>Alles selecteren</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.ui" line="40"/>
         <source>Uncheck All</source>
-        <translation type="unfinished"></translation>
+        <translation>Niets selecteren</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.ui" line="60"/>
         <location filename="../app/mainwindow.cpp" line="107"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Hulp</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.ui" line="67"/>
         <location filename="../app/mainwindow.cpp" line="70"/>
         <source>System Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Systeeminstellingen</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.ui" line="74"/>
         <source>Register Association</source>
-        <translation type="unfinished"></translation>
+        <translation>Toewijzing vastleggen</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="27"/>
         <source>File Association Manager for %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Bestandstoewijzingen beheren voor %1</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="38"/>
         <source>Registered: %1 | Associated: %2 format(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>Vastgelegd: %1 | Toegewezen: %2 formaat/formaten</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="39"/>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>Ja</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="39"/>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>Nee</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="69"/>
         <location filename="../app/mainwindow.cpp" line="79"/>
         <source>Success</source>
-        <translation type="unfinished"></translation>
+        <translation>Voltooid</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="74"/>
         <source>Skip</source>
-        <translation type="unfinished"></translation>
+        <translation>Overslaan</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="75"/>
         <source>Associations information updated successfully.</source>
-        <translation type="unfinished"></translation>
+        <translation>De toewijzingsinformatie is bijgewerkt.</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="76"/>
         <source>You might also want to open System Settings&apos; default apps management page. Open now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Mogelijk wil je dit ook aanpassen in de systeeminstellingen. Wil je die nu openen?</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="79"/>
         <source>Associations information removed successfully.</source>
-        <translation type="unfinished"></translation>
+        <translation>De toewijzingsinformatie is verwijderd.</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="94"/>
         <source>This program helps you register file type assoication information and capabilities to system register.</source>
-        <translation type="unfinished"></translation>
+        <translation>Met behulp van dit programma kun je bestandstypen toewijzen aan bepaalde programma&apos;s.</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="95"/>
         <source>When you click the &quot;Register Association&quot; button, if you have checked at least one file type association, this program will
 register `%1` to your system, with the file type association capabilities you&apos;ve selected.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klik op de knop ‘Toewijzing vastleggen’ als je minimaal één bestandstype hebt gekozen. Dit programma zal vervolgens
+‘%1’ vastleggen in de systeeminstellingen, met alle gekozen mogelijkheden.</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="97"/>
         <source>Due to Windows 10+ limitation, **if you already associated the selected formats with other program, then we cannot directly modify
 existing file type association for you**, so after you done the registration by clicking &quot;Register Association&quot;, you can then click
 the &quot;System Settings&quot; button to directly open the system control panel for `%1`, so you can manage the file type association there.</source>
-        <translation type="unfinished"></translation>
+        <translation>Door een beperking op Windows 10 en nieuwer, **kun je geen formaten aanpassen die reeds zijn toegewezen aan andere programma&apos;s**.
+Na het klikken op de vastlegknop, kun je daarom de systeeminstellingen voor ‘%1’ openen en
+het bestandstype in kwestie aldaar beheren.</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="100"/>
         <source>When you want to remove the system registration for `%1`, you can simply uncheck all file types on the left-hand side,
 then click &quot;Register Association&quot; again. It will remove all related information from the system registery.</source>
-        <translation type="unfinished"></translation>
+        <translation>Als je de instellingen voor ‘%1’ wilt verwijderen, klik dan op de knop ‘Niets selecteren’ en vervolgens
+nogmaals op de vastlegknop. Hierdoor wordt alle bijbehorende informatie uit het systeemregister verwijderd.</translation>
     </message>
 </context>
 <context>
@@ -111,23 +115,23 @@ then click &quot;Register Association&quot; again. It will remove all related in
     <message>
         <location filename="../app/main.cpp" line="23"/>
         <source>Pineapple Assoc Manager</source>
-        <translation type="unfinished"></translation>
+        <translation>Pineapple-toewijzingsbeheer</translation>
     </message>
     <message>
         <location filename="../app/main.cpp" line="31"/>
         <source>Path to configuration file.</source>
-        <translation type="unfinished"></translation>
+        <translation>De locatie van het instellingenbestand.</translation>
     </message>
     <message>
         <location filename="../app/main.cpp" line="32"/>
         <location filename="../app/main.cpp" line="36"/>
         <source>file</source>
-        <translation type="unfinished"></translation>
+        <translation>bestand</translation>
     </message>
     <message>
         <location filename="../app/main.cpp" line="35"/>
         <source>Path to .pademo/.patest file.</source>
-        <translation type="unfinished"></translation>
+        <translation>De locatie van het .pademo-/.patest-bestand.</translation>
     </message>
 </context>
 </TS>
