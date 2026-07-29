171115b4b26dfc559f567efb0f6337be2a6c66a3
Hosted Weblate <hosted@weblate.org>
2026-07-29 12:01:55 +0200

    i18n: Translations update from Hosted Weblate
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Oğuz Ersen <oguz@ersen.moe>
    Co-authored-by: Tech-Tac <ahmad.tech.tac@gmail.com>
    Translate-URL: https://hosted.weblate.org/projects/pineapple-pictures/assoc-manager/ar/
    Translate-URL: https://hosted.weblate.org/projects/pineapple-pictures/assoc-manager/tr/
    Translation: pineapple-pictures/File Association Manager

--- translations/pineapple-assoc-manager_ar.ts
+++ translations/pineapple-assoc-manager_ar.ts
@@ -1,113 +1,109 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="tr" sourcelanguage="en">
+<TS version="2.1" language="ar" sourcelanguage="en">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="../app/mainwindow.ui" line="14"/>
         <source>File Association Manager</source>
-        <translation>Dosya İlişki Yöneticisi</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/mainwindow.ui" line="33"/>
         <source>Check All</source>
-        <translation>Tümünü İşaretle</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/mainwindow.ui" line="40"/>
         <source>Uncheck All</source>
-        <translation>Tüm İşaretleri Kaldır</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/mainwindow.ui" line="60"/>
         <location filename="../app/mainwindow.cpp" line="107"/>
         <source>Help</source>
-        <translation>Yardım</translation>
+        <translation>المساعدة</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.ui" line="67"/>
         <location filename="../app/mainwindow.cpp" line="70"/>
         <source>System Settings</source>
-        <translation>Sistem Ayarları</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/mainwindow.ui" line="74"/>
         <source>Register Association</source>
-        <translation>İlişki Kaydet</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="27"/>
         <source>File Association Manager for %1</source>
-        <translation>%1 için Dosya İlişki Yöneticisi</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="38"/>
         <source>Registered: %1 | Associated: %2 format(s)</source>
-        <translation>Kaydedilen: %1 | İlişkilendirilen: %2 biçim</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="39"/>
         <source>Yes</source>
-        <translation>Evet</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="39"/>
         <source>No</source>
-        <translation>Hayır</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="69"/>
         <location filename="../app/mainwindow.cpp" line="79"/>
         <source>Success</source>
-        <translation>Başarılı</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="74"/>
         <source>Skip</source>
-        <translation>Atla</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="75"/>
         <source>Associations information updated successfully.</source>
-        <translation>İlişki bilgisi güncellendi.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="76"/>
         <source>You might also want to open System Settings&apos; default apps management page. Open now?</source>
-        <translation>Ayrıca Sistem Ayarlarındaki öntanımlı uygulamalar yönetim sayfasını da açmak isteyebilirsiniz. Şimdi açılsın mı?</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="79"/>
         <source>Associations information removed successfully.</source>
-        <translation>İlişki bilgisi kaldırıldı.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="94"/>
         <source>This program helps you register file type assoication information and capabilities to system register.</source>
-        <translation>Bu program, dosya türü ilişkilendirme bilgilerini ve özelliklerini sistem kayıt defterine kaydetmenize yardımcı olur.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="95"/>
         <source>When you click the &quot;Register Association&quot; button, if you have checked at least one file type association, this program will
 register `%1` to your system, with the file type association capabilities you&apos;ve selected.</source>
-        <translation>“İlişki Kaydet” düğmesine tıkladığınızda, en az bir dosya türü ilişkilendirmesini işaretlediyseniz, bu program
-`%1` değerini, seçtiğiniz dosya türü ilişkilendirme özellikleriyle birlikte sisteminize kaydedecektir.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="97"/>
         <source>Due to Windows 10+ limitation, **if you already associated the selected formats with other program, then we cannot directly modify
 existing file type association for you**, so after you done the registration by clicking &quot;Register Association&quot;, you can then click
 the &quot;System Settings&quot; button to directly open the system control panel for `%1`, so you can manage the file type association there.</source>
-        <translation>Windows 10 ve üzeri sürümlerdeki kısıtlama nedeniyle, **seçtiğiniz biçimleri başka bir programla zaten ilişkilendirdiyseniz, var olan dosya
-türü ilişkilendirmelerini sizin adınıza doğrudan değiştiremeyiz**, bu nedenle &quot;İlişki Kaydet&quot; düğmesine tıklayarak kaydı tamamladıktan sonra,
-&quot;Sistem Ayarları&quot; düğmesine tıklayarak `%1` için sistem denetim panelini doğrudan açabilir ve dosya türü ilişkilendirmelerini buradan yönetebilirsiniz.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="100"/>
         <source>When you want to remove the system registration for `%1`, you can simply uncheck all file types on the left-hand side,
 then click &quot;Register Association&quot; again. It will remove all related information from the system registery.</source>
-        <translation>`%1` için sistem kayıtlarını silmek istediğinizde, sol taraftaki tüm dosya türlerinin işaretini kaldırıp ardından &quot;İlişki Kaydet&quot;
-düğmesine tekrar tıklayabilirsiniz. Bu, sistem kayıt defterinden ilgili tüm bilgileri silecektir.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -115,23 +111,23 @@ düğmesine tekrar tıklayabilirsiniz. Bu, sistem kayıt defterinden ilgili tüm
     <message>
         <location filename="../app/main.cpp" line="23"/>
         <source>Pineapple Assoc Manager</source>
-        <translation>Pineapple İlişki Yöneticisi</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/main.cpp" line="31"/>
         <source>Path to configuration file.</source>
-        <translation>Yapılandırma dosyasının yolu.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/main.cpp" line="32"/>
         <location filename="../app/main.cpp" line="36"/>
         <source>file</source>
-        <translation>dosya</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../app/main.cpp" line="35"/>
         <source>Path to .pademo/.patest file.</source>
-        <translation>.pademo/.patest dosyasının yolu.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
