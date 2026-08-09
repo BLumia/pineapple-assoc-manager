394a00d31786bb77583a854c386da82ae31033ac
Hosted Weblate <hosted@weblate.org>
2026-08-09 05:14:16 +0200

    i18n: Translations update from Hosted Weblate
    Co-authored-by: Tech-Tac <ahmad.tech.tac@gmail.com>
    Translate-URL: https://hosted.weblate.org/projects/pineapple-pictures/assoc-manager/ar/
    Translation: pineapple-pictures/File Association Manager

--- translations/pineapple-assoc-manager_ar.ts
+++ translations/pineapple-assoc-manager_ar.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../app/mainwindow.ui" line="14"/>
         <source>File Association Manager</source>
-        <translation type="unfinished"></translation>
+        <translation>مدير ارتباطات الملفات</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.ui" line="33"/>
         <source>Check All</source>
-        <translation type="unfinished"></translation>
+        <translation>تحديد الكل</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.ui" line="40"/>
         <source>Uncheck All</source>
-        <translation type="unfinished"></translation>
+        <translation>إلغاء تحديد الكل</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.ui" line="60"/>
@@ -28,82 +28,83 @@
         <location filename="../app/mainwindow.ui" line="67"/>
         <location filename="../app/mainwindow.cpp" line="70"/>
         <source>System Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>إعدادات النظام</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.ui" line="74"/>
         <source>Register Association</source>
-        <translation type="unfinished"></translation>
+        <translation>تسجيل ارتباط</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="27"/>
         <source>File Association Manager for %1</source>
-        <translation type="unfinished"></translation>
+        <translation>مدير ارتباطات الملفات لـ%1</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="38"/>
         <source>Registered: %1 | Associated: %2 format(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>مسجل: %1 | مرتبط: %2 من التنسيقات</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="39"/>
         <source>Yes</source>
-        <translation type="unfinished"></translation>
+        <translation>نعم</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="39"/>
         <source>No</source>
-        <translation type="unfinished"></translation>
+        <translation>لا</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="69"/>
         <location filename="../app/mainwindow.cpp" line="79"/>
         <source>Success</source>
-        <translation type="unfinished"></translation>
+        <translation>نجاح</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="74"/>
         <source>Skip</source>
-        <translation type="unfinished"></translation>
+        <translation>تخطي</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="75"/>
         <source>Associations information updated successfully.</source>
-        <translation type="unfinished"></translation>
+        <translation>تم تحديث معلومات الارتباطات بنجاح.</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="76"/>
         <source>You might also want to open System Settings&apos; default apps management page. Open now?</source>
-        <translation type="unfinished"></translation>
+        <translation>قد تريد أيضًا فتح صفحة إدارة التطبيقات الخاصة بإعدادات النظام، هل تريد فتحها الآن؟</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="79"/>
         <source>Associations information removed successfully.</source>
-        <translation type="unfinished"></translation>
+        <translation>تم إزالة معلومات الارتباطات بنجاح.</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="94"/>
         <source>This program helps you register file type assoication information and capabilities to system register.</source>
-        <translation type="unfinished"></translation>
+        <translation>هذا البرنامج يساعدك على تسجيل ارتباطات أنواع الملفات في سجل النظام.</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="95"/>
         <source>When you click the &quot;Register Association&quot; button, if you have checked at least one file type association, this program will
 register `%1` to your system, with the file type association capabilities you&apos;ve selected.</source>
-        <translation type="unfinished"></translation>
+        <translation>عندما تنقر زر &quot;تسجيل الارتباط&quot; وهناك نوع أو أكثر من نوع ملف محدد، سيتم تسجيل `%1` في نظامك مع ارتباطات الملفات التي اخترتها.</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="97"/>
         <source>Due to Windows 10+ limitation, **if you already associated the selected formats with other program, then we cannot directly modify
 existing file type association for you**, so after you done the registration by clicking &quot;Register Association&quot;, you can then click
 the &quot;System Settings&quot; button to directly open the system control panel for `%1`, so you can manage the file type association there.</source>
-        <translation type="unfinished"></translation>
+        <translation>نظرًا لقيود في نظام التشغيل Windows 10 وما بعده، **إذا قمت بربط التنسيقات المحددة مع برنامج آخر، لن نتمكن من تعديل تلك الارتباطات بشكل مباشر**، لذا بعد أن تنتهي من تسجيلهم عبر نقر &quot;تسجيل الارتباط&quot;، يمكنك أن تنفر زر &quot;إعدادات النظام&quot; لفتح لوحة التحكم الخاصة بالنظام النظام لـ%1 حتى تتمكن منها من إدارة ارتباطات أنواع الملفات.</translation>
     </message>
     <message>
         <location filename="../app/mainwindow.cpp" line="100"/>
         <source>When you want to remove the system registration for `%1`, you can simply uncheck all file types on the left-hand side,
 then click &quot;Register Association&quot; again. It will remove all related information from the system registery.</source>
-        <translation type="unfinished"></translation>
+        <translation>عندما تريد أن تزيل سجلات النظام الخاصة بـ`%1`، يمكنك فقط إلغاء تحديد كل أنواع الملفات على اليمين،
+ثم النقر على &quot;تسجيل الارتباط&quot; مرة أخرى. سيتم إزالة كل المعلومات المتعلقة من سجلات النظام.</translation>
     </message>
 </context>
 <context>
@@ -111,23 +112,23 @@ then click &quot;Register Association&quot; again. It will remove all related in
     <message>
         <location filename="../app/main.cpp" line="23"/>
         <source>Pineapple Assoc Manager</source>
-        <translation type="unfinished"></translation>
+        <translation>مدير ارتباطات بَيْنَأبِل</translation>
     </message>
     <message>
         <location filename="../app/main.cpp" line="31"/>
         <source>Path to configuration file.</source>
-        <translation type="unfinished"></translation>
+        <translation>مسار ملف التكوين.</translation>
     </message>
     <message>
         <location filename="../app/main.cpp" line="32"/>
         <location filename="../app/main.cpp" line="36"/>
         <source>file</source>
-        <translation type="unfinished"></translation>
+        <translation>ملف</translation>
     </message>
     <message>
         <location filename="../app/main.cpp" line="35"/>
         <source>Path to .pademo/.patest file.</source>
-        <translation type="unfinished"></translation>
+        <translation>مسار ملف `.pademo/.patest`.</translation>
     </message>
 </context>
 </TS>
